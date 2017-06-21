4a61e8807733ebede431b8d7a85900ae2f348c7a
DeshanKTD <deshankalupahana@gmail.com>
2017-06-21 19:28:00 +0530

    database initializaion

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('fname');
+            $table->string('lname');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('address');
+            $table->string('telephone');
+            $table->string('company');
+            $table->string('role');
             $table->rememberToken();
             $table->timestamps();
         });
